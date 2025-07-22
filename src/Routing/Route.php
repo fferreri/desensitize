7d42cbf9c9f4f68e29cff1616311f3a5147aa14c
Federico Ferreri <fferreri@users.noreply.github.com>
2025-07-21 21:48:40 -0300

    Update Route.php

--- src/Routing/Route.php
+++ src/Routing/Route.php
@@ -4,9 +4,40 @@ namespace Waryor\Desensitize\Routing;
 
 use Illuminate\Http\Request;
 use Waryor\Desensitize\Validator\DesensitizedUriValidator;
+use Symfony\Component\Routing\CompiledRoute;
 
 class Route extends \Illuminate\Routing\Route
 {
+    /**
+     * Compile the route.
+     *
+     * @return \Symfony\Component\Routing\CompiledRoute
+     */
+    protected function compile()
+    {
+        // Primero, dejamos que la clase padre haga su compilación normal.
+        $compiled = parent::compile();
+
+        // Si la ruta no está marcada como sensible a mayúsculas, modificamos su regex.
+        if (empty($this->action['case_sensitive'])) {
+            $caseInsensitiveRegex = rtrim($compiled->getRegex(), 'sDu') . 'sDui';
+
+            // Creamos una nueva instancia de CompiledRoute con nuestra regex modificada.
+            $compiled = new CompiledRoute(
+                $compiled->getStaticPrefix(),
+                $caseInsensitiveRegex,
+                $compiled->getTokens(),
+                $compiled->getPathVariables(),
+                $compiled->getHostRegex(),
+                $compiled->getHostTokens(),
+                $compiled->getHostVariables(),
+                $compiled->getVariables()
+            );
+        }
+
+        return $compiled;
+    }
+    
     /**
      * Bind the route to a given request for execution.
      *
